6d16bfe3ba4e418292ea5baf5fdbcd4655e2bef2
Kartik Sharma <poojan4604@gmail.com>
2025-11-16 16:31:54 +0530

    Update Showcase.jsx

--- frontend/src/components/Showcase.jsx
+++ frontend/src/components/Showcase.jsx
@@ -1,8 +1,8 @@
 const Showcase=()=>{
     return(
-        <section id="showcase">
+        <section id="showacase">
             <div className="media">
-                <video src="/videos/game.mp4 loop muted autoPlay playsInline"/>
+                <video src="/videos/game.mp4" loop muted autoPlay playsInline/>
                 <div className="mask">
                     <img src="/mask-logo.svg"/>
                 </div>
@@ -12,7 +12,18 @@ const Showcase=()=>{
                     <div className="lg:max-w-md">\
                         <h2>Rocket Chip</h2>
                         <div className="space-y-5 mt-7 pe-10">
-                            <p>Intro</p>
+                            <p>Introducing 
+                                <span className="text-white">
+                                    M4, the next generation of Apple Silicon
+                                </span>
+                                . M4 powers
+                            </p>
+                            <p>
+                                It drives Apple Intelligence on iPad Pro, so you 
+                            </p>
+                            <p>
+                                A brand-new display engine delivers breath-taking 
+                            </p>
                         </div>
                     </div>
                 </div>
