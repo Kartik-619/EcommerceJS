4116f011e3bbaf254b3b78b9ab59e1b94aabc450
Kartik Sharma <poojan4604@gmail.com>
2025-12-30 19:18:06 +0530

    cart in backend

--- frontend/src/components/checkout/CheckOutForm.jsx
+++ frontend/src/components/checkout/CheckOutForm.jsx
@@ -1,7 +1,10 @@
+import useUserStore from "../../store/userStore";
+
 const CheckOutForm=()=>{
+    const cart = useUserStore((state) => state.cart);
     return(
-        <div>
-            <form className=" bg-white border-2 border-black rounded-xl p-8  space-y-6 w-full max-w-md">
+        <div className="h-full w-full bg-white flex items-center justify-center p-4">
+            <form className=" bg-slate-300 border-2 border-black rounded-xl p-8  space-y-6 w-full max-w-md">
                 <input
                     placeholder="username"
                     type="name"
@@ -32,8 +35,18 @@ const CheckOutForm=()=>{
                     className="w-full bg-white border border-black text-black rounded-lg px-4 py-3 focus:outline-none focus:border-[#00f0ff] focus:shadow-[0_0_10px_#00f0ff] transition-all"
 
                 />
+                <h2>List of items :</h2>
+                <ul>
+                    {
+                        cart.map((i)=>(
+                            <li key={i.productId}>{i.productId}*{i.quantity}</li>
+                        ))
+                    }
+                </ul>
                
-                   
+                   <button
+                    className="w-full bg-white border border-black text-black rounded-lg px-4 py-3 focus:outline-none focus:border-[#00f0ff] focus:shadow-[0_0_10px_#00f0ff] transition-all"
+                   >Proceed to Pay</button>
             </form>
         </div>
     )
