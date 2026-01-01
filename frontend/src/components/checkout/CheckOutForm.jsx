f7332cbd1a017696e6dfa18c1cdf9f2f13589198
Kartik Sharma <poojan4604@gmail.com>
2026-01-01 22:39:44 +0530

    css fixes

--- frontend/src/components/checkout/CheckOutForm.jsx
+++ frontend/src/components/checkout/CheckOutForm.jsx
@@ -8,14 +8,14 @@ const CheckOutForm = () => {
 
   return (
     <div className="min-h-screen w-full bg-neutral-50 flex items-center justify-center px-4">
-      <form className="w-full max-w-md rounded-2xl bg-white p-8 shadow-lg space-y-6">
+      <form className="w-full max-w-2xl  h-full  max-h-xl rounded-2xl bg-white p-8 shadow-lg space-y-6">
         
         {/* Header */}
         <div className="space-y-1">
           <h2 className="text-2xl font-semibold text-black">
             Checkout
           </h2>
-          <p className="text-sm text-neutral-500">
+          <p className="text-md text-neutral-500">
             Enter your delivery and contact details
           </p>
         </div>
@@ -54,11 +54,14 @@ const CheckOutForm = () => {
         </div>
 
         {/* CTA */}
+        <h3 className="e px-4 py-3 text-md text-black ">*Proceed to see the order summary
+
+        </h3>
         <button
           type="submit"
           className="w-full rounded-lg bg-black py-3 text-sm font-medium text-white transition hover:bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-black/20"
         >
-          Proceed to Payment
+          Proceed for Summary
         </button>
       </form>
     </div>
--- frontend/src/components/checkout/checkOut.jsx
+++ frontend/src/components/checkout/checkOut.jsx
@@ -3,7 +3,7 @@ import CheckOutForm from "./CheckOutForm";
 const CheckOutPage=()=>{
     return(
         <div className="min-h-screen w-full bg-white text-black pt-24">
-            <h3 className="text-3xl bold">Fill in the details to order the items in the cart!!</h3>
+            <h3 className="pt-10 pl-10 text-3xl bold">Fill in the details to order the items in the cart!!</h3>
 
             <CheckOutForm/>
 
