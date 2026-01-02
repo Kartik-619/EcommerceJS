593fb139af876210a9251173e41d793de35363a2
Kartik Sharma <poojan4604@gmail.com>
2026-01-02 11:02:02 +0530

    Update CheckOutForm.jsx

--- frontend/src/components/checkout/CheckOutForm.jsx
+++ frontend/src/components/checkout/CheckOutForm.jsx
@@ -1,14 +1,27 @@
 import useUserStore from "../../store/userStore";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const inputClass =
   "w-full rounded-lg border border-neutral-300 bg-white px-4 py-3 text-sm text-black placeholder-neutral-500 transition-all focus:outline-none focus:border-black focus:ring-2 focus:ring-black/10";
 
 const CheckOutForm = () => {
+//calling the navigate function
+const navigate=useNavigate();
+//all the local states
   const cart = useUserStore((state) => state.cart);
+  const [Fullname,setFullname]=useState('');
+  const [number,setMobileNumber]=useState(Number);
+  const[address,setAddress]=useState('');
+  const HandleCheckout=()=>{
+    alert('Order summary is being prepared');
+    navigate('/ordersummary');
+
+  }
 
   return (
     <div className="min-h-screen w-full bg-neutral-50 flex items-center justify-center px-4">
-      <form className="w-full max-w-2xl  h-full  max-h-xl rounded-2xl bg-white p-8 shadow-lg space-y-6">
+      <form onSubmit={HandleCheckout} className="w-full max-w-2xl  h-full  max-h-xl rounded-2xl bg-white p-8 shadow-lg space-y-6"  >
         
         {/* Header */}
         <div className="space-y-1">
