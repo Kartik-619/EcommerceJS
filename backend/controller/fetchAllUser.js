9dc0e1a76143a97457a11dc8028750897d3f9534
Kartik Sharma <poojan4604@gmail.com>
2026-02-01 14:50:21 +0530

    Create fetchAllUser.js

---
+++ backend/controller/fetchAllUser.js
@@ -0,0 +1,37 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const fetchAllUser=async(req,res)=>{
+    try{
+        const users=await prisma.user.findMany({
+            select:{
+                username:true,
+                email:true,
+                role:true,
+                orderItems:{
+                    select:{
+                        id:true,
+                        quantity:true,
+                        price:true,
+                        product:{
+                            select:{
+                                slug:true,
+                                model:true,
+                                basePrice:true
+                            }
+                        }
+                    }
+                },
+               
+            }
+        });
+        res.json(users);
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"Failed to fetch all users"
+        });
+    }
+}
+
+exports.module=fetchAllUser;
